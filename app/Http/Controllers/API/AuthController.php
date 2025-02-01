f869c8349f10568e533426c63c1aa1e7c263568c
Mikeee2001 <occ.macas.michael@gmail.com>
2025-02-01 08:29:49 +0800

    Add existing files

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+      $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'validation_errors' =>$validator->messages(),
+            ]);
+        }
+        else
+        {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+        }
+        $token = $user->createToken( $user->email.'_Token')->plainTextToken;
+        
+        return response()->json([
+        'status' => 200,
+        'username' => $user->name,
+        'token' => $token,
+        'message' => 'User registered successfully',
+        ]);
+    } 
+
+
+
+    public function login(Request $request)
+    {
+       $validator = Validator::make($request->all(),[
+            'email' => 'required|max:191',
+            'password' => 'required',
+       ]);
+
+       if($validator ->fails())
+       {
+        return response()->json([
+            'validation_errors' =>$validator->messages(),
+        ]);
+       }
+       else
+       {
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) 
+        {
+            return response()->json([
+                'status' =>401,
+                'message' => 'Invalid Credentials',
+            ]);
+        }
+        else
+        {
+            $token = $user->createToken( $user->email.'_Token')->plainTextToken;
+        
+            return response()->json([
+            'status' => 200,
+            'username' => $user->name,
+            'token' => $token,
+            'message' => 'User Logged in successfully',
+            ]);
+        }
+       }
+    }
+
+}
