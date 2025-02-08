a9bbe8552dc52000175cb5406f552c23ffa37d2d
Mikeee2001 <occ.macas.michael@gmail.com>
2025-02-08 23:20:15 +0800

    2-8-2025

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -12,74 +12,92 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
-      $validator = Validator::make($request->all(), [
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
 
-        if($validator->fails())
-        {
+        if ($validator->fails()) {
             return response()->json([
-                'validation_errors' =>$validator->messages(),
+                'validation_errors' => $validator->messages(),
             ]);
-        }
-        else
-        {
+        } else {
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
             ]);
         }
-        $token = $user->createToken( $user->email.'_Token')->plainTextToken;
-        
+        $token = $user->createToken($user->email . '_Token')->plainTextToken;
+
         return response()->json([
-        'status' => 200,
-        'username' => $user->name,
-        'token' => $token,
-        'message' => 'User registered successfully',
+            'status' => 200,
+            'username' => $user->name,
+            'role' => $user->role,
+            'token' => $token,
+            'message' => 'User registered successfully',
         ]);
-    } 
+    }
 
 
 
     public function login(Request $request)
     {
-       $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'email' => 'required|max:191',
             'password' => 'required',
-       ]);
 
-       if($validator ->fails())
-       {
-        return response()->json([
-            'validation_errors' =>$validator->messages(),
         ]);
-       }
-       else
-       {
-        $user = User::where('email', $request->email)->first();
- 
-        if (! $user || ! Hash::check($request->password, $user->password)) 
-        {
+
+        if ($validator->fails()) {
             return response()->json([
-                'status' =>401,
-                'message' => 'Invalid Credentials',
+                'validation_errors' => $validator->messages(),
             ]);
+        } else {
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Credentials',
+                ]);
+
+
+            } 
+            else
+
+             {
+
+                if ($user->role_as == 1) //1 is equal to admin
+                {
+                    $role = 'admin';
+                    $token = $user->createToken($user->email.'_AdminToken', ['server:admin'])->plainTextToken;
+                }
+                 else 
+                 {
+                    $role = '';
+                    $token = $user->createToken($user->email . '_Token', [''])->plainTextToken;
+                }
+
+                return response()->json([
+                    'status' => 200,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'message' => 'User Logged in successfully',
+                    'role' => $role,
+                ]);
+            }
         }
-        else
-        {
-            $token = $user->createToken( $user->email.'_Token')->plainTextToken;
-        
-            return response()->json([
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
             'status' => 200,
-            'username' => $user->name,
-            'token' => $token,
-            'message' => 'User Logged in successfully',
-            ]);
-        }
-       }
+            'message' => 'Logged out successfully',
+        ]);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,40 @@ use App\Http\Controllers\API\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 //login and register api
 Route::post("/register", [AuthController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
+
+//admin route in the admin it will check the authentication of your and admin
+Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group(function() {
+
+    Route::get('/checkingAuthenticated', function() {
+        return response()->json(['message' => 'You are in', 'status' => 200], 200);
+
+    });
+ 
+});
+
+// //admin route in the admin it will check the authentication of your and admin
+// Route::middleware(['auth:sanctum', 'isAPIDoctor'])->group(function() {
+
+//     Route::get('/checkingAuthenticated', function() {
+//         return response()->json(['message' => 'Welcome Doctor', 'status' => 200], 200);
+
+//     });
+ 
+// });
+
+
+//user route
+Route::middleware(['auth:sanctum'])->group(function() {
+    
+
+    Route::post('/logout', [AuthController::class, "logout"]);
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
